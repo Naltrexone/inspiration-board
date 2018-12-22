777d5150456f76bd8fca725cb54c0ac8c9bd3fc3
Divya <divyamohanan@gmail.com>
2018-12-22 14:26:17 -0800

    Refactoring done

--- src/App.js
+++ src/App.js
@@ -1,85 +1,17 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 import './App.css';
 import Board from './components/Board';
-import Status from './components/Status';
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      boardName: 'Divya',
-      boards: [],
-      boardsUrl: 'https://inspiration-board.herokuapp.com/boards/',
-      status: {
-        message: 'Successfully loaded the page',
-        type: 'success'
-      }
-    };
-  }
-
-  updateStatus = (message, type) => {
-    this.setState({
-      status: {
-        message: message,
-        type: type
-      }
-    });
-  }
-
-  componentDidMount() {
-    this.updateStatus('Retrieving inspirational boards', 'success');
-
-    axios.get(this.state.boardsUrl)
-      .then((response) => {
-        this.updateStatus('Successfully retrieved all boards', 'success');
-        let updatedBoards = response.data.map((boardInfo) => {
-          return boardInfo.board;
-        });
-        this.setState({
-          boards: updatedBoards
-        });
-      })
-      .catch((error) => {
-        this.updateStatus(`There was a problem retrieving the boards: ${error.message}`, 'error');
-      });
-  }
-
-  onBoardSelectionChange = (event) => {
-    let updatedBoardName = {
-      boardName: event.target.value
-    };
-    this.setState(updatedBoardName);
-  }
-
   render() {
-    const boardOptions = this.state.boards.map((board) => {
-      return <option key={board.id}>{board.name}</option>
-    });
     return (
       <section>
         <header className="header">
-          <h1 className="header__h1"><span className="header__text">Inspiration Board</span></h1>
+          <h1 className="header__h1"><span className="header__text">Inspire</span></h1>
         </header>
-        <Status
-          message={ this.state.status.message }
-          type={ this.state.status.type }
-        />
-
-        <section className="board-selection">
-          <select
-            className="board-selection__select"
-            onChange={ this.onBoardSelectionChange }
-            value={ this.state.boardName }
-          >
-            {boardOptions}
-          </select>
-        </section>
-
         <Board
-          url={ this.state.boardsUrl }
-          boardName={ this.state.boardName }
-          updateStatusCallback={ this.updateStatus }
+          url="https://inspiration-board.herokuapp.com/boards/"
+          boardName={'Divya'}
           />
       </section>
     );
